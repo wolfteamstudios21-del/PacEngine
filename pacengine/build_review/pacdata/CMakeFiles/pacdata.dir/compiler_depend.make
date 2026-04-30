aa72b5ad9066e980dbcc2955e26c8e1c02a0e4b7
wolfteamstudios <56675463-wolfteamstudios@users.noreply.replit.com>
2026-04-30 04:37:41 +0000

    Git commit prior to merge

--- pacengine/build_review/pacdata/CMakeFiles/pacdata.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for pacdata.
-# This may be replaced when dependencies are built.
--- pacengine/build_review/render/CMakeFiles/pacengine_shaders.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pacengine_shaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
